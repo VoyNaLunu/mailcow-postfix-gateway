8ffbb0f5cdeaca95719d51db384e2a4219f680cd
VoyNaLunu <lalalalaskir@gmail.com>
2023-05-10 15:06:30 +0300

    inital commit

---
+++ build/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function modify_relay_config():
+    key = ${1}
+    value = ${2}
+    if [ -z "${key}"] || [ -z "${value}"]; then
+        echo "you have to set both key and value" && exit 1
+    fi
+    postconf -e "${key} = ${value}"
+
+modify_relay_config myhostname "${MAILCOW_HOSTNAME}"
+modify_relay_config mydomain "${MAILCOW_DOMAIN}"
+modify_relay_config relayhost "[${MAILCOW_IP:-172.22.1.253}]:${SMTPS_PORT}"
+modify_relay_config "mynetworks" "${MAILCOW_IPV4_NETWORK:-172.22.1.0/24} ${MAILCOW_IPV6_NETWORK:-fd4d:6169:6c63:6f77::/64} ${WG_NETWORK:-100.64.0.0/24} 127.0.0.0/8"
+modify_relay_config "smtp_sasl_auth_enable" "yes"
+modify_relay_config "smtp_sasl_security_options" "noanonymous"
+modify_relay_config "smtp_sasl_password_maps" "hash:/etc/postfix/sasl_passwd"
+modify_relay_config "smtp_use_tls" "yes"
+modify_relay_config "smtp_host_lookup" "native,dns"
+modify_relay_config "smtp_tls_wrappermode" "yes"
+modify_relay_config "smtp_tls_security_level" "encrypt"
+modify_relay_config "inet_protocols" "all"
+
+
+exec /usr/sbin/postfix -c /etc/postfix start-fg
