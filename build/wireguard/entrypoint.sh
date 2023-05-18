6f959fecd89637d8c7864812bca4241e28a166f1
VoyNaLunu <lalalalaskir@gmail.com>
2023-05-18 16:23:05 +0300

    removed unnecessary iptables rule

--- build/wireguard/entrypoint.sh
+++ build/wireguard/entrypoint.sh
@@ -15,14 +15,13 @@ if [ ! -s /etc/wireguard/wg0.conf ]; then
 
     echo -e "[Interface]\nListenPort = 51820\nAddress = ${GATEWAY_WG_NETWORK:-100.64.0}.1/24" > /etc/wireguard/wg0.conf
     echo -e "PrivateKey = $(cat gateway-privatekey)" >> /etc/wireguard/wg0.conf
-    echo -e "[Peer]\nAllowedIPs = ${GATEWAY_WG_NETWORK:-100.64.0}.2/24\nPublicKey = ${CLIENT_WG_PUBKEY}" >> /etc/wireguard/wg0.conf
+    echo -e "[Peer]\nAllowedIPs = ${GATEWAY_WG_NETWORK:-100.64.0}.0/24\nPublicKey = ${CLIENT_WG_PUBKEY}" >> /etc/wireguard/wg0.conf
     chmod 700 /etc/wireguard/wg0.conf
 fi
 
 wg-quick up wg0 && echo -e "\n"
 
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
 iptables -t nat -A PREROUTING -i wg0 -p tcp --dport ${GATEWAY_SMTP_PORT:-25} -j DNAT --to-destination $(dig +short gateway-postfix):25
 
 wg && echo -e "\n"
